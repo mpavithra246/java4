541cfbab8a08a6b635fcdad5d38a4ddc901fd2f5
mpavithra246 <mpavithra246@gmail.com>
2016-08-10 11:28:38 +0530

    fgbfd
    fdwrf

---
+++ largenum.java
@@ -0,0 +1,21 @@
+public class lagge3 {
+
+	
+	public static void main(String[] args) {
+	int a=Integer.parseInt(args[0]);
+	int b=Integer.parseInt(args[1]);
+	int c=Integer.parseInt(args[2]);
+	if(a>b&&a>c)
+	{
+		System.out.println(a);
+	}
+	else if(b>c)
+	{
+		System.out.println(b);
+	}
+	else
+	{
+		System.out.println(c);
+	}
+	}
+}
