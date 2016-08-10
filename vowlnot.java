52b402909c4a6b6d43cd40ade63aec4df984bfe8
mpavithra246 <mpavithra246@gmail.com>
2016-08-10 10:31:52 +0530

    hbf
    hsdbf

---
+++ vowlnot.java
@@ -0,0 +1,15 @@
+public class vowelnot {
+
+	public static void main(String[] args) {
+		char c='i';
+		if(c=='a'||c=='e'||c=='i'||c=='0'||c=='u')
+		{
+			System.out.println("the character is vowel");
+		}
+		else
+		{
+			System.out.println("the character is not a vowel");
+		}
+}
+
+}
